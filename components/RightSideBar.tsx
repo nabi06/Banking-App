cadeb88b02ecec118d6b47097c0bb6284b0a6589
Niyaz Nabi <niyaznabi@Niyazs-Air.lan>
2024-09-17 18:51:21 -0500

    19 commit

--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'lucide-react'
 import React from 'react'
 
 const RightSideBar = ({user,transactions,banks}) => {
@@ -6,7 +7,7 @@ const RightSideBar = ({user,transactions,banks}) => {
       <aside className='right-sidebar'>
         <section className='flex flex-col pb-8'>
             <div className='profile-banner'>
-                <div className='profile-img'>
+                <div className='profile-img absolute '>
                     <span className='text-5xl text-blue-500 font-bold'>{user.firstName[0].toUpperCase()}</span>
 
                 </div>
@@ -14,6 +15,14 @@ const RightSideBar = ({user,transactions,banks}) => {
                     <h1 className='profile-name'>{user.firstName}</h1>
                     <p className='profile-email'>{user.email}</p>
                 </div>
+            </div>
+        </section>
+        <section className='banks '>
+            <div className='flex w-full justify-between'>
+                <h2 className='header-2'>My Banks</h2>
+                <Link href='/' className='flex gap-2'>
+                
+                </Link>
             </div>
         </section>
       </aside>
